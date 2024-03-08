f98a544ba3cf10b7e1f1665e1a21aff1828ce99c
Uday <budayteja009@gmail.com>
2024-03-08 22:29:43 +0530

    Add cors

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// enable CORS for all requests
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 /**
  * Middleware that logs the time for every request.
  * @param {express.Request} req - The Express request object.
