7936aecd0ffa543cd4719b43e4f8a60f86b31625
Uday <budayteja009@gmail.com>
2024-03-09 18:17:28 +0530

    Add cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, X-Access-Token');
   next();
 });
 
--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -37,11 +37,5 @@ module.exports = (app) => {
    * 
    * @returns {void}
    */
-  app.get("/v1/bookings", [authUser.verifyToken], (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
-
-    bookingController.getBookingsByUser(req, res);
-  });
+  app.get("/v1/bookings", [authUser.verifyToken], bookingController.getBookingsByUser);
 };
