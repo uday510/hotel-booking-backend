e9d6b6984e7d6fea9319f2de0e53c9c050c8288f
Uday <budayteja009@gmail.com>
2024-03-09 18:06:42 +0530

    add cors header for x-access-token

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
   next();
 });
 
